94628c9b8d4312232bd9383114767d3a2b194607
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-03 21:27:34 +0100

    les endpoint de l'entité paiement

--- Routes/paiement.js
+++ Routes/paiement.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const Bdd = require("../bdd/connexion");
 const moment = require("moment");
 
+// Création d'un paiement
 router.post("/nouvpaiment", (req, res) => {
   const numpaie = "PAIE" + "_" + Date.now();
   const datepai = moment(Date.now()).format("DD-MM-YYYY");
   const sql =
-    "INSERT INTO paiement(`numPaiement`, `montatPaiement`, `motifPaiement`, `datePaiement`, `apprenant`) VALUES(?)";
+    "INSERT INTO paiement(`numPaiement`, `montantPaiement`, `motifPaiement`, `datePaiement`, `apprenant`) VALUES(?)";
   const values = [
     numpaie,
     req.body.montant,
@@ -21,6 +22,7 @@ router.post("/nouvpaiment", (req, res) => {
   });
 });
 
+// Afficher tous les paiements
 router.get("/paiements", (req, res) => {
   const sql = "SELECT * FROM paiement";
   Bdd.query(sql, (err, resultat) => {
@@ -29,6 +31,7 @@ router.get("/paiements", (req, res) => {
   });
 });
 
+// Modification d'un paiement
 router.put("/modifpaiement/:numPaiement", (req, res) => {
   const numpaie = req.params.numPaiement;
   const sql =
@@ -44,6 +47,49 @@ router.put("/modifpaiement/:numPaiement", (req, res) => {
   );
 });
 
+// Recherche d'un paiement par rapoort à une date
+router.get("/paiements/date/:datePaiament", (req, res) => {
+  const datePaie = req.params.datePaiament;
+  const sql = "SELECT * FROM paiement  WHERE datePaiement = ?";
+
+  Bdd.query(sql, [datePaie], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+
+// Recherche des paiements effectués aujourd'hui
+router.get("/paiements/aujourdhui/:datePaiament", (req, res) => {
+  const datePaie = moment(Date.now()).format("DD-MM-YYYY");
+  const sql = "SELECT * FROM paiement  WHERE datePaiement = ?";
+
+  Bdd.query(sql, [datePaie], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+
+// Recherche des paiements par étudiant
+router.get("/paiement/apprenant/:apprenant", (req, res) => {
+  const apprenant = req.params.apprenant;
+  const sql = "SELECT * FROM paiement  WHERE apprenant = ?";
+
+  Bdd.query(sql, [apprenant], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+router.get("/paiement/date/:datePaiament", (req, res) => {
+  const datePaie = req.params.datePaiament;
+  const sql = "SELECT * FROM paiement  WHERE datePaiement = ?";
+
+  Bdd.query(sql, [datePaie], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+
+// suppression d'un paiement
 router.delete("/suppaiement/:numPaiement", (req, res) => {
   const numpaie = req.params.numPaiement;
   const sql = "DELETE FROM paiement WHERE numPaiement = ?";
