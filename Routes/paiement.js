8905264ec96f3ef1c1b132d36f18c024932b3c93
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-03 16:10:23 +0100

    fhfnf

---
+++ Routes/paiement.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const Bdd = require("../bdd/connexion");
+
+
+router.get('/nouvpaiement', (req, res)=>{
+    const numpaie = PAIE + "_" + Date.now()
+    
+})
