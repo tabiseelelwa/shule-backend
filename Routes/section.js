d08c15693c8daf7b243ab8047f0472c117fd3193
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-03 15:30:09 +0100

    traitement de la route de section

--- Routes/section.js
+++ Routes/section.js
@@ -5,14 +5,30 @@ const Bdd = require("../bdd/connexion");
 router.post("/nouvsection", (req, res) => {
   const code = Date.now();
   const values = [code, req.body.designation];
-  const sql =
-    "INSERT INTO section(`codeSection`, `designSection`) VALUES(?)";
+  const sql = "INSERT INTO section(`codeSection`, `designSection`) VALUES(?)";
   Bdd.query(sql, [values], (err, resultat) => {
     if (err) return res.json(err);
     return res.json({
-    //   resultat,
+      //   resultat,
       message: "Enregistrement effectué avec succès",
     });
+  });
+});
+
+router.get("/sections", (req, res) => {
+  const sql = "SELECT * FROM section";
+  Bdd.query(sql, (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+
+router.get("/section/:codeSection", (req, res) => {
+  const code = req.params.codeSection;
+  const sql = "SELECT * FROM section WHERE codeSection = ?";
+  Bdd.query(sql, [code], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json({ message: "Modification effectué avec succès" });
   });
 });
 
