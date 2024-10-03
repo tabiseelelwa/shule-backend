ea0f808c59b18d383bf04d7c680023d2e2366870
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-03 19:39:11 +0100

    hjdshjdshj

--- Routes/paiement.js
+++ Routes/paiement.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Bdd = require("../bdd/connexion");
+const moment = require("moment");
 
-
-router.get('/nouvpaiement', (req, res)=>{
-    const numpaie = PAIE + "_" + Date.now()
-    
-})
+router.post("/nouvpaiement", (req, res) => {
+  const numpaie = PAIE + "_" + Date.now();
+  const datepai = moment(Date.now()).format("DD-MM-YYYY");
+  const sql =
+    "INSERT INTO paiement(numpaiem, montantpaiement, motif, datepaie) ";
+  const values = [numpaie, req.body.montant, req.body.motif, datepai];
+  Bdd.query(sql, [values], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json({ Message: "Enregistrement confirmé..." });
+  });
+});
