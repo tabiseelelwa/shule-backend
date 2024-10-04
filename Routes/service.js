9f0b966246ea69377cd8a53c2a35408f396f5d95
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-04 15:07:37 +0100

    Les routes de 'Service'

--- Routes/service.js
+++ Routes/service.js
@@ -13,8 +13,36 @@ router.post("/nouvserv", (req, res) => {
   });
 });
 
-router.get('/services', (req, res)=>{
-    const requete = ""
-})
+router.get("/services", (req, res) => {
+  const requete = "SELECT * FROM service";
+  Bdd.query(requete, (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+
+router.get("/service/:codeService", (req, res) => {
+  const code = req.params.codeService;
+  const requete = "SELECT * FROM service WHERE codeService = ?";
+  Bdd.query(requete, [code], (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
+  });
+});
+
+router.put("/modifservice/:codeService", (req, res) => {
+  const code = req.params.codeService;
+  const requete =
+    "UPDATE service SET designService = ?, descService = ? WHERE codeService = ?";
+
+  Bdd.query(
+    requete,
+    [req.body.designation, req.body.description, code],
+    (err, resultat) => {
+      if (err) return res.json(err);
+      return res.json({ resultat, Message: "Modifié...!" });
+    }
+  );
+});
 
 module.exports = router;
