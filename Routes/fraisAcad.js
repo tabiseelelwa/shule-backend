ae5a97ea222b3ede9a1e603477949cedb83d69d2
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-06 18:43:21 +0100

    construction de la route de lecture des frais

--- Routes/fraisAcad.js
+++ Routes/fraisAcad.js
@@ -5,11 +5,19 @@ const Bdd = require("../bdd/connexion");
 router.post("/nouvfrais", (req, res) => {
   const code = Date.now();
   const requete =
-    "INSERT INTO  fraisacad(idFrais, montFrais, statut) VALUES(?)";
-  const values = [code, req.body.montant];
+    "INSERT INTO  fraisacad(idFrais, designFrais, montFrais) VALUES(?)";
+  const values = [code, req.body.designation, req.body.montant];
   Bdd.query(requete, [values], (err) => {
     if (err) return res.json(err);
     return res.json({ Message: "Enregistré.....!" });
+  });
+});
+
+router.get("/list/frais", (req, res) => {
+  const requete = "SELECT * FROM fraisacad";
+  Bdd.query(requete, (err, resultat) => {
+    if (err) return res.json(err);
+    return res.json(resultat);
   });
 });
 
