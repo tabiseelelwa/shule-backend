c3513760058cbe5feac2c83c6dc00c2c144e7925
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-06 18:22:41 +0100

    kjefhjdf

--- Routes/fraisAcad.js
+++ Routes/fraisAcad.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Bdd = require("../bdd/connexion");
 
-router.post('/nouvfaris', (req, res)=>{
-    
-})
+router.post("/nouvfaris", (req, res) => {
+  const code = Date.now();
+  const requete =
+    "INSERT INTO  fraisacad(idFrais, montFrais, statut) VALUES(?)";
+  const values = [code, req.body.montant];
+  Bdd.query(requete, [values], (err) => {
+    if (err) return res.json(err);
+    return res.json({ Message: "Enregistré.....!" });
+  });
+});
 
 module.exports = router;
