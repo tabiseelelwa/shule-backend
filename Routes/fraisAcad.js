d242dda869ce09bf97e341e23ec926ea0562e94f
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-10-06 18:17:14 +0100

    creation frais

---
+++ Routes/fraisAcad.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const Bdd = require("../bdd/connexion");
+
+router.post('/nouvfaris', (req, res)=>{
+    
+})
+
+module.exports = router;
